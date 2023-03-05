3b288133ee17f82f0c909937cec400ddb6cd49c2
Allisson Mateus <allissonmateus89@gmail.com>
2023-03-04 23:05:26 -0300

    Init

--- src/API.ts
+++ src/API.ts
@@ -1,12 +1,47 @@
 import express from 'express'
+import axios from 'axios'
 const app = express()
 
 app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.status(200).json({
-    ok: true
-  })
+app.post('/webhook-gateway-pagamento', async function (req, res) {
+
+
+  
+  try{
+    const data = req.body
+
+
+    switch(data['type']) {
+      case "payment":
+
+        await axios({
+          url: 'https://www.uplifeoriginal.com.br/webhook-gateway-pagamento',
+          method: 'post',
+          data,
+          headers:{
+            'Content-Type' : 'application/json'
+          }
+        })
+    
+      
+        break;
+      default:
+        return res.status(200).json({
+          ok: true
+        })
+    }
+
+
+    return res.status(200).json({
+      ok: true
+    })
+  }catch(err : any){
+    console.log('err', err.message)
+    res.status(500).json({
+      ok : false
+    })
+  }
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
